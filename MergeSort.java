419290c144e38fbdc73334d00f76914d70d4df8b
Jsaket058 <97850136+Jsaket058@users.noreply.github.com>
2024-02-26 19:29:48 +0530

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class MergeSort{
+	void merge(int arr[],int low,int mid,int high){
+	    ArrayList<Integer> temp = new ArrayList<>();
+		int left=low;
+		int right=mid+1;
+        while(left<=mid && right<=high){
+			if(arr[left]<arr[right]){
+				temp.add(arr[left]);
+				left++;
+			}
+			else{
+				temp.add(arr[right]);
+				right++;
+			}
+		}
+        while(left<=mid){
+			temp.add(arr[left]);
+			left++;
+		}
+        while(right<=high){
+			temp.add(arr[right]);
+			right++;
+		}
+        
+        for(int i=low;i<=high;i++){
+			arr[i]=temp.get(i-low);
+		}		
+	}
+	
+	void mergeSort(int arr[],int low,int high){
+		if(low>=high)return;
+		int mid=(low+high)/2;
+		mergeSort(arr,low,mid);
+		mergeSort(arr,mid+1,high);
+		merge(arr,low,mid,high);
+	}
+}
+class Run{
+	public static void main(String[] args){
+		MergeSort m=new MergeSort();
+		int arr[]={5,4,2,9,1,7,8};
+		m.mergeSort(arr,0,arr.length-1);
+		for(int i : arr){
+			System.out.println(i);
+		}
+	}
+}
